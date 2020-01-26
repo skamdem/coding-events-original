218ff3e7a3da1e3093bfc627b3e442962b50fffb
sylvain kamdem <sylvain.kamdem@gmail.com>
2020-01-26 18:17:11 -0500

    small modification

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,8 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -17,11 +17,15 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
+        Map<String, String> events = new HashMap<String, String>();
+        events.put("C plus plus", "C++ in finance");
+        events.put("java", "Golf Code with Java");
+        events.put("javascript", "Essentials of java Script in web development");
+        //events.put("software architects", "Object oriented code Soft architects");
+        /*events.add("Code With Pride");
         events.add("Strange Loop");
         events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+        events.add("SpringOne Platform");*/
         model.addAttribute("events", events);
         return "events/index";
     }
