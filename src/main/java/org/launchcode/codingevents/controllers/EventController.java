ac5a69bd94ea755d8cee8c2134efab104eb955be
sylvain kamdem <sylvain.kamdem@gmail.com>
2020-01-26 21:05:58 -0500

    completed excercice

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,15 +19,33 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        Map<String, String> events = new HashMap<String, String>();
-        events.put("C plus plus", "C++ in finance");
-        events.put("java", "Golf Code with Java");
-        events.put("javascript", "Essentials of java Script in web development");
+        List<HashMap<String, String>> events = new ArrayList<HashMap<String, String>>();
+        String[] keys = new String[]{"language","description","image"};
+        String[] tableHeader = new String[]{"Event Language","Description","Image"};
+        String[] imgPaths = new String[]{"/images/c++.png","/images/java.png","/images/javascript.png"};
+        HashMap<String, String> event1 = new HashMap<String, String>();
+        HashMap<String, String> event2 = new HashMap<String, String>();
+        HashMap<String, String> event3 = new HashMap<String, String>();
+
+        event1.put(keys[0],"C plus plus");
+        event1.put(keys[1], "C++ in finance");
+        event1.put(keys[2], imgPaths[0]);
+
+        event2.put(keys[0],"java");
+        event2.put(keys[1], "Golf Code with Java");
+        event2.put(keys[2], imgPaths[1]);
+
+        event3.put(keys[0],"javascript");
+        event3.put(keys[1], "Essentials of java Script in web development");
+        event3.put(keys[2], imgPaths[2]);
+
+        events.add(event1);
+        events.add(event2);
+        events.add(event3);
+
         //events.put("software architects", "Object oriented code Soft architects");
-        /*events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");*/
+        model.addAttribute("keys", keys);
+        model.addAttribute("tableHeader", tableHeader);
         model.addAttribute("events", events);
         return "events/index";
     }
